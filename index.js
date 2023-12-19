aad412388fba2c0a42195051a24b257a82b24948
Himanshu <himanshujainhj70662@gmail.com>
2023-12-20 00:46:54 +0530

    done

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+// const port = process.env.Port || 8080;
+
+app.use(express.static(__dirname));
+
+// app.get('/',(req,res)=>{
+//     res.sendFile('index.html',{root:__dirname});
+// });
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get('/about',(req,res)=>{
+    res.sendFile('about.html',{root:__dirname});
+});
+
+app.use((req,res)=>{
+    res.status(404).sendFile('404.html',{root:__dirname});
+});
+
+
+app.listen(port,()=>{
+    console.log('EXpress app is listening at port 3000 ');
+});
